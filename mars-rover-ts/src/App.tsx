1180841018074343633795e394c5bcef12c5dfc3
Jamie Wallace <jwallacedev@gmail.com>
2023-01-11 17:03:28 +0000

    Render a row of squares - all still within App

--- mars-rover-ts/src/App.tsx
+++ mars-rover-ts/src/App.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  let row = [];
+
+  for (let index = 0; index < 10; index++) {
+      row.push(<div key={index} className='square'></div>);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <div data-testid='square1' className='square'></div>
+        <div data-testid='row_1'>
+          {row}
+        </div>
       </header>
     </div>
   );
--- mars-rover-ts/src/__tests__/MarsRover.test.tsx
+++ mars-rover-ts/src/__tests__/MarsRover.test.tsx
@@ -4,7 +4,8 @@ import App from '../App';
 describe('MarsRover', () => {
     test('renders a grid', () => {
         render(<App />);
-        const square1 = screen.getByTestId('square1');
-        expect(square1).toBeInTheDocument();
+
+        const row1 = screen.getByTestId('row_1');
+        expect(row1.childNodes).toHaveLength(10);
     });
 });
