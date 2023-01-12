0954b1f0689b18982d512167ee26e203fb6e8ce9
Jamie Wallace <jwallacedev@gmail.com>
2023-01-12 13:56:26 +0000

    Make Grid a class

--- mars-rover-ts/src/grid/Grid.tsx
+++ mars-rover-ts/src/grid/Grid.tsx
@@ -1,14 +1,14 @@
+import React from 'react';
 import { Row } from './Row';
 
-interface GridProps {
-}
+export class Grid extends React.Component {
+    render() {
+        let rows = [];
 
-export function Grid(props: GridProps) {
-    let rows = [];
+        for (let index = 0; index < 10; index++) {
+            rows.push(<Row key={index} />);
+        }
 
-    for (let index = 0; index < 10; index++) {
-        rows.push(<Row key={index} />);
+        return <div data-testid='mars-rover-grid'>{rows}</div>;
     }
-
-    return <div data-testid='mars-rover-grid'>{rows}</div>;
 }
