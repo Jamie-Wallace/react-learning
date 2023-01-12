c64e02683dd7e5a17a25d316d1283b2b94a547b0
Jamie Wallace <jwallacedev@gmail.com>
2023-01-12 11:00:26 +0000

    Tidy up

--- mars-rover-ts/src/App.tsx
+++ mars-rover-ts/src/App.tsx
@@ -1,15 +1,7 @@
-import React from 'react';
 import './App.css';
 import { Row } from './grid/Row';
-import {Square} from './grid/Square';
 
 function App() {
-  let row = [];
-
-  for (let index = 0; index < 10; index++) {
-      row.push(<Square key={index} value="-"/>);
-  }
-
   return (
     <div className='App'>
       <header className='App-header'>
