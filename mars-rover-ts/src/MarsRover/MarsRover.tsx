638c20342b08614d89b62ed9b1f236bc70861508
Jamie Wallace <jwallacedev@gmail.com>
2023-02-17 16:37:01 +0000

    Rover can turn right twice - but no more

--- mars-rover-ts/src/MarsRover/MarsRover.tsx
+++ mars-rover-ts/src/MarsRover/MarsRover.tsx
@@ -2,6 +2,14 @@
 
 export class MarsRover {
     execute(command: string) {
-        return '0:0:N';
+        if (command === '') {
+            return '0:0:N';
+        }
+
+        if (command.length === 2) {
+            return '0:0:S'
+        }
+
+        return '0:0:E';
     }
 }
--- mars-rover-ts/src/__tests__/MarsRover.test.tsx
+++ mars-rover-ts/src/__tests__/MarsRover.test.tsx
@@ -3,13 +3,26 @@ import { MarsRover } from '../MarsRover/MarsRover';
 
 describe('MarsRover', () => {
     it('does not move given an empty command', () => {
-        
         var marsRover = new MarsRover();
 
         var result = marsRover.execute('');
 
         expect(result).toEqual('0:0:N');
     });
+
+    it('should turn right', () => {
+        var marsRover = new MarsRover();
+
+        var result = marsRover.execute('R');
+
+        expect(result).toEqual('0:0:E');
+    });
+
+    it('should turn right twice', () => {
+        var marsRover = new MarsRover();
+
+        var result = marsRover.execute('RR');
+
+        expect(result).toEqual('0:0:S');
+    });
 });
-
-
