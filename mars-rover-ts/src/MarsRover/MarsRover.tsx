05639cd99958c75575a9d4d62a55a45712c7fb1a
Daniel Russom <daniel.russom00@gmail.com>
2023-02-21 16:38:23 +0000

    Turn left 5 times + refactor again - extracting functions

--- mars-rover-ts/src/MarsRover/MarsRover.tsx
+++ mars-rover-ts/src/MarsRover/MarsRover.tsx
@@ -5,23 +5,44 @@ export class MarsRover {
         let directions = ['N', 'E', 'S', 'W'];
 
         let directionIndex = 0;
-
         Array.from(command).forEach(character => {
-            if (character === 'L') {
-                directionIndex--;
+            if (commandIsTurnLeft(character)) {
+                directionIndex = turnLeft(directionIndex);
             }
-            if (character === 'R') {
-                directionIndex++;
-            }
-            if (directionIndex < 0) {
-                directionIndex = 3;
-            }
-            if (directionIndex > 3) {
-                directionIndex = 0;
+            if (commandIsTurnRight(character)) {
+                directionIndex = turnRight(directionIndex);
             }
         });
 
         const direction = directions[directionIndex];
         return `0:0:${direction}`;
     }
+}
+
+function commandIsTurnLeft(commandCharacter: string): boolean {
+    return commandCharacter === 'L';
+}
+
+function commandIsTurnRight(commandCharacter: string): boolean {
+    return commandCharacter === 'R';
+}
+
+function turnLeft(directionIndex: number): number {
+    directionIndex--;
+
+    if (directionIndex < 0) {
+        directionIndex = 3;
+    }
+
+    return directionIndex;
+}
+
+function turnRight(directionIndex: number): number {
+    directionIndex++;
+
+    if (directionIndex > 3) {
+        directionIndex = 0;
+    }
+
+    return directionIndex;
 }
