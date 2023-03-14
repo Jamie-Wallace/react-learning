5018db6d1df82571722e2dae6234c184ca4f2434
Daniel Russom <daniel.russom00@gmail.com>
2023-03-14 16:53:39 +0000

    Moving the rover git p

--- mars-rover-ts/src/App.tsx
+++ mars-rover-ts/src/App.tsx
@@ -2,20 +2,34 @@ import { MouseEventHandler, useState } from 'react';
 import './App.css';
 import { Grid } from './grid/Grid';
 import { MarsRover } from './MarsRover/MarsRover';
+import { Position } from './MarsRover/Position';
 
 export const App = () => {
   const [location, setLocation] = useState("");
+  const [squares, setSquares] = useState([['^', '', ''], ['', '', ''], ['', '', '']]);
+  const [lastPosition, setLastPosition] = useState(new Position('N', 0, 0));
 
   const moveRover: MouseEventHandler = () => {
       let marsRover = new MarsRover();
+
+      
       let result = marsRover.execute('M');
+
+      var newSquares = [...squares]; //[['', '', ''], ['', '', ''], ['', '', '']]
+
+      newSquares[lastPosition.positionY][lastPosition.positionX] = '';
+      newSquares[result.positionY][result.positionX] = '^';
+
+
+      setSquares(newSquares);//.reverse());
+      setLastPosition(result);
       setLocation(`${result.positionX}:${result.positionY}:${result.compass}`);
     }
 
  return <div className='App'>
  <header className='App-header'>
    <div>
-     <Grid squares={[['', '^'], ['', '']]} />
+     <Grid squares={squares} />
      <div>
        <button onClick={moveRover}>Move</button>
      </div>
