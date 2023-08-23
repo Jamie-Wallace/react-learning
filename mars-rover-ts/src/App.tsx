941e9b9e0a7e428814dcd078e7026b2e93ea06f5
Jamie Wallace <jwallacedev@gmail.com>
2023-08-23 16:58:03 +0100

    Generate grid squares don't hard code

--- mars-rover-ts/src/App.tsx
+++ mars-rover-ts/src/App.tsx
@@ -1,4 +1,4 @@
-import { MouseEventHandler, useState } from "react";
+import React, { MouseEventHandler, useState } from "react";
 import "./App.css";
 import { Grid } from "./grid/Grid";
 import { Square } from "./grid/Square";
@@ -9,30 +9,25 @@ import { Coordinates } from "./MarsRover/Coordinates";
 import { Direction } from "./MarsRover/Direction";
 
 export const App = () => {
+  const gridSize = 10;
+
+  function createGridSquares() {
+    let gridRows: Array<Array<React.ReactElement>> = [];
+    for (let rows = 0; rows < gridSize; rows++) {
+      let gridColumns: Array<React.ReactElement> = [];
+      for (let columns = 0; columns < gridSize; columns++) {
+        gridColumns.push(<Square/>);
+      }
+      gridRows.push(gridColumns);
+    }
+
+    gridRows[0][0] = <MarsRoverComponent direction={Direction.North}/>;
+
+    return gridRows;
+  }
+
   const [location, setLocation] = useState("");
-  const [squares, setSquares] = useState([
-    [
-      <MarsRoverComponent direction={Direction.North} />,
-      <Square />,
-      <Square />,
-      <Square />,
-      <Square />,
-      <Square />,
-      <Square />,
-      <Square />,
-      <Square />,
-      <Square />
-    ],
-    [<Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />],
-    [<Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />],
-    [<Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />],
-    [<Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />],
-    [<Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />],
-    [<Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />],
-    [<Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />],
-    [<Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />],
-    [<Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />, <Square />]
-  ]);
+  const [squares, setSquares] = useState(createGridSquares());
   const [lastPosition, setLastPosition] = useState(
     new Position(Direction.North, new Coordinates(0, 0))
   );
