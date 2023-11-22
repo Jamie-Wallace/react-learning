3546f7c72af09b6f7507d91831854ca556fe0c46
Jamie Wallace <jwallacedev@gmail.com>
2023-11-22 17:01:53 +0000

    Refactor and todo

--- mars-rover-ts-2/src/MarsRoverController.test.ts
+++ mars-rover-ts-2/src/MarsRoverController.test.ts
@@ -2,23 +2,21 @@ import { MarsRoverController } from "./MarsRoverController";
 
 describe("MarsRoverController", () => {
   it('should start at position', () => {
-
-    // TODO: execute("M")???
     let controller = new MarsRoverController();
 
     expect(controller.getPosition()).toBe("0, 0");
   });
 
   it('should move', () => {
-    // TODO: execute("M")???
     let controller = new MarsRoverController();
+    // TODO: UP TO HERE:
+    // Change to execute("M") - so is in line with kata.
     controller.move();
 
     expect(controller.getPosition()).toBe("0, 1");
   });
 
   it('should move twice', () => {
-    // TODO: execute("M")???
     let controller = new MarsRoverController();
     controller.move();
     controller.move();
--- mars-rover-ts-2/src/MarsRoverController.ts
+++ mars-rover-ts-2/src/MarsRoverController.ts
@@ -1,10 +1,8 @@
 export class MarsRoverController {
-  private position = "0, 0";
   private yPosition = 0;
 
   move() {
     this.yPosition+=1;
-    this.position = "0, 1";
   }
 
   turnRight() {
