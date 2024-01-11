bbef84d9668a325da7d4caf90bb72654775004a1
Daniel Russom <daniel.russom00@gmail.com>
2024-01-11 12:34:45 +0000

    Add notes for next time

--- mars-rover-ts-2/src/MarsRoverController.ts
+++ mars-rover-ts-2/src/MarsRoverController.ts
@@ -1,22 +1,5 @@
 export class MarsRoverController {
-  private yPosition = 0;
   execute(command: string) {
     throw new Error("Not implemented!");
-  }
-
-  move() {
-    this.yPosition += 1;
-  }
-
-  turnRight() {
-    throw new Error("Not implemented!");
-  }
-
-  turnLeft() {
-    throw new Error("Not implemented!");
-  }
-
-  getPosition() {
-    return `0, ${this.yPosition}`;
   }
 }
