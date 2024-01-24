f29141580df908404145943e1b9e8c8a8a49623a
Jamie Wallace <jwallacedev@gmail.com>
2024-01-24 16:43:49 +0000

    Refactor of rover code

--- mars-rover-ts-2/src/MarsRoverController.ts
+++ mars-rover-ts-2/src/MarsRoverController.ts
@@ -1,8 +1,7 @@
 export class MarsRoverController {
   execute(command: string) {
-    if (command.length == 2) return "S";
-    if (command.length == 3) return "W";
-    if (command.length == 4) return "N";
-    return "E";
+    const directions = ["E", "S", "W", "N"];
+
+    return directions[command.length - 1];
   }
 }
