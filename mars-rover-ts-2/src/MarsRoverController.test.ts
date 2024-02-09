d77618bf91343efaf1b95af2e4483984e1696ea9
Daniel Russom <daniel.russom00@gmail.com>
2024-02-09 11:00:07 +0000

    MRC: Turn left twice

--- mars-rover-ts-2/src/MarsRoverController.test.ts
+++ mars-rover-ts-2/src/MarsRoverController.test.ts
@@ -2,14 +2,14 @@ import { MarsRoverController } from "./MarsRoverController";
 
 describe("MarsRoverController", () => {
   it.each([
-          ["R", "E"],
-          ["RR", "S"],
-          ["RRR", "W"],
-          ["RRRR", "N"],
-          ["RRRRR", "E"],
-          ["RRRRRR", "S"],
-          ["RRRRRRRRRR", "S"]])
-  ('should turn right', (command, expectedDirection) => {
+    ["R", "E"],
+    ["RR", "S"],
+    ["RRR", "W"],
+    ["RRRR", "N"],
+    ["RRRRR", "E"],
+    ["RRRRRR", "S"],
+    ["RRRRRRRRRR", "S"],
+  ])("should turn right", (command, expectedDirection) => {
     let controller = new MarsRoverController();
 
     let direction = controller.execute(command);
@@ -18,8 +18,9 @@ describe("MarsRoverController", () => {
   });
 
   it.each([
-    ["L", "W"]])
-  ('should turn left', (command, expectedDirection) => {
+    ["L", "W"],
+    ["LL", "S"],
+  ])("should turn left", (command, expectedDirection) => {
     let controller = new MarsRoverController();
 
     let direction = controller.execute(command);
