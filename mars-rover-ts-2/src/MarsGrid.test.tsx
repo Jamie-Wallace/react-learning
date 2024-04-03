8d1df5bb7e3892f03c01a364b81d5b7e6db7658e
Daniel Russom <daniel.russom00@gmail.com>
2024-04-03 16:29:47 +0100

    Parameterize y coord tests

--- mars-rover-ts-2/src/MarsGrid.test.tsx
+++ mars-rover-ts-2/src/MarsGrid.test.tsx
@@ -2,23 +2,14 @@ import { render, screen } from "@testing-library/react";
 import MarsGrid from "./MarsGrid";
 
 describe("MarsGrid should", () => {
-  it("render a square at x0 y0", () => {
-    render(<MarsGrid />);
+  it.each([0, 1, 2])(
+    "render a square at x0 in the expected y position",
+    (yPosition) => {
+      render(<MarsGrid />);
 
-    expect(screen.getByLabelText("square at x0 y0")).toBeVisible();
-  });
-
-  it("render a square at x0 y1", () => {
-    render(<MarsGrid />);
-
-    expect(screen.getByLabelText("square at x0 y1")).toBeVisible();
-  });
-
-  it("render a square at x0 y2", () => {
-    render(<MarsGrid />);
-
-    expect(screen.getByLabelText("square at x0 y2")).toBeVisible();
-  });
+      expect(screen.getByLabelText(`square at x0 y${yPosition}`)).toBeVisible();
+    }
+  );
 
   // render all squares
 
