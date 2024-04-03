29220f95a5c3b24cf1b37077a493e08a06e29ed4
Daniel Russom <daniel.russom00@gmail.com>
2024-04-03 16:33:29 +0100

    Remove duplication of y coord creation

--- mars-rover-ts-2/src/MarsGrid.tsx
+++ mars-rover-ts-2/src/MarsGrid.tsx
@@ -1,11 +1,10 @@
 function MarsGrid() {
-  return (
-    <>
-      <span aria-label="square at x0 y0"></span>
-      <span aria-label="square at x0 y1"></span>
-      <span aria-label="square at x0 y2"></span>
-    </>
-  );
+  let squares = [];
+  for (let i = 0; i <= 2; i++) {
+    squares.push(<span aria-label={`square at x0 y${i}`}></span>);
+  }
+
+  return <>{squares}</>;
 }
 
 export default MarsGrid;
