8dc8a592b8265b494cae676ec514f1923fb31248
Daniel Russom <daniel.russom00@gmail.com>
2024-04-03 16:23:21 +0100

    Render square 0,1

--- mars-rover-ts-2/src/MarsGrid.test.tsx
+++ mars-rover-ts-2/src/MarsGrid.test.tsx
@@ -7,4 +7,14 @@ describe("MarsGrid should", () => {
 
     expect(screen.getByLabelText("square at x0 y0")).toBeVisible();
   });
+
+  it("render a square at x0 y1", () => {
+    render(<MarsGrid />);
+
+    expect(screen.getByLabelText("square at x0 y1")).toBeVisible();
+  });
+
+  // render all squares
+
+  // check squares display nothing
 });
--- mars-rover-ts-2/src/MarsGrid.tsx
+++ mars-rover-ts-2/src/MarsGrid.tsx
@@ -1,5 +1,10 @@
 function MarsGrid() {
-  return <span aria-label="square at x0 y0"></span>;
+  return (
+    <>
+      <span aria-label="square at x0 y0"></span>
+      <span aria-label="square at x0 y1"></span>
+    </>
+  );
 }
 
 export default MarsGrid;
