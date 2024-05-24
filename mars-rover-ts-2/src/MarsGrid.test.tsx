2c39939573ddbdb8a596141141b48d296acdeee4
Daniel Russom <daniel.russom00@gmail.com>
2024-05-24 11:09:06 +0100

    Missed commit whoops

--- mars-rover-ts-2/src/MarsGrid.test.tsx
+++ mars-rover-ts-2/src/MarsGrid.test.tsx
@@ -1,11 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import MarsGrid from "./MarsGrid";
+import { Position } from "./Position";
+import { Coordinate } from "./Coordinate";
+import { Compass } from "./Compass";
 
 describe("MarsGrid should", () => {
   it.each([0, 1, 2, 3, 9])(
     "render squares with the expected y coordinate",
     (yPosition) => {
-      render(<MarsGrid position={""} />);
+      render(
+        <MarsGrid position={new Position(new Coordinate(), new Compass())} />
+      );
 
       expect(screen.getByLabelText(`square at x0 y${yPosition}`)).toBeVisible();
       expect(screen.getByLabelText(`square at x9 y${yPosition}`)).toBeVisible();
@@ -13,14 +18,18 @@ describe("MarsGrid should", () => {
   );
 
   it("render an empty square", () => {
-    render(<MarsGrid position={""} />);
+    render(
+      <MarsGrid position={new Position(new Coordinate(), new Compass())} />
+    );
 
     let square = screen.getByLabelText("square at x0 y0");
     expect(square).toHaveTextContent("");
   });
 
   it.skip("render the rover at the starting position", () => {
-    render(<MarsGrid position={"0"} />);
+    render(
+      <MarsGrid position={new Position(new Coordinate(), new Compass())} />
+    );
 
     let square = screen.getByLabelText("square at x0 y0");
     expect(square).toHaveTextContent("^");
