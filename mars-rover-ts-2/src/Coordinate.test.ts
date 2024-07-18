3b3ee7cebf17f438aa66017bbc7ea752608ac931
Jamie Wallace <jwallacedev@gmail.com>
2024-07-18 11:08:45 +0100

    Refactor test to move North

--- mars-rover-ts-2/src/Coordinate.test.ts
+++ mars-rover-ts-2/src/Coordinate.test.ts
@@ -1,19 +1,16 @@
 import { Coordinate } from "./Coordinate";
-import { start } from "repl";
 
 describe("Coordinate should", () => {
   it.each([
-    [0, 1],
-    [1, 2],
-    [9, 0],
+    [new Coordinate(0, 0), new Coordinate(0, 1)],
+    [new Coordinate(5, 1), new Coordinate(5, 2)],
+    [new Coordinate(0, 9), new Coordinate(0, 0)]
   ])(
       "Move north to expected coordinate",
       (startingCoordinate, expectedCoordinate) => {
-        const coordinate = new Coordinate(0, startingCoordinate);
+        const result = Coordinate.moveNorth(startingCoordinate);
 
-        const result = Coordinate.moveNorth(coordinate);
-
-        expect(result.getCurrentCoordinate()).toBe(`0:${expectedCoordinate}`);
+        expect(result).toStrictEqual(expectedCoordinate);
       }
   );
 
