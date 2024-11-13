21fda086813224f412d9174bbf9e4bdfb2b4991d
jamiewallace <jwallacedev@gmail.com>
2024-11-13 13:58:23 +0000

    Remove unnecessary test

--- great-circle-distance/src/customer-selector.test.ts
+++ great-circle-distance/src/customer-selector.test.ts
@@ -2,19 +2,6 @@ import {expect, test} from "vitest";
 import {CustomerSelector} from "./customer-selector.ts";
 import {Customer} from "./customer.ts";
 
-test('returns unfiltered list of customers', () => {
-    const inputCustomers: Customer[] = [
-        new Customer(),
-        new Customer(),
-        new Customer()];
-
-    const customerSelector = new CustomerSelector();
-
-    const selectedCustomers = customerSelector.selectCustomers(inputCustomers, 0);
-
-    expect(selectedCustomers).toEqual(inputCustomers);
-});
-
 test('selects one customer', () => {
     let customer1 = new Customer(-6.2592576, 53.3381985);
 
