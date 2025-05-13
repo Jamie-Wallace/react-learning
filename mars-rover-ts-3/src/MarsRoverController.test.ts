e2c3d826a96957ebda00ca51b4911efb73bccf0b
jamiewallace <jwallacedev@gmail.com>
2025-05-13 16:05:09 +0100

    Failing controller test for one left command

---
+++ mars-rover-ts-3/src/MarsRoverController.test.ts
@@ -0,0 +1,22 @@
+import {MarsRoverController} from "./MarsRoverController.ts";
+
+describe("MarsRoverController", () => {
+    it.each([
+        ["L", "W"],
+        // ["LL", "S"],
+        // ["LLL", "E"],
+        // ["LLLL", "N"],
+        // ["LLLLL", "W"],
+        // ["LLLLLL", "S"],
+        // ["LLLLLLLLLL", "S"],
+    ])(
+        "when command is %s, should turn left to face %s",
+        (command, expectedDirection) => {
+            let controller = new MarsRoverController();
+
+            let direction = controller.execute(command);
+
+            expect(direction).toBe(expectedDirection);
+        }
+    );
+});
