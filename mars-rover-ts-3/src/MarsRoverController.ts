756f4ef66d2d1aba14ae898998254d4f8ec808a4
Hazel Russom <hazel.russom@codurance.com>
2025-07-08 15:46:38 +0100

    Refactor turn left

--- mars-rover-ts-3/src/MarsRoverController.ts
+++ mars-rover-ts-3/src/MarsRoverController.ts
@@ -1,12 +1,8 @@
 export class MarsRoverController {
 
   execute(command: string) {
-    if (command === "L") {
-      return "W";
-    } else if (command === "LL") {
-      return "S";
-    } else if (command === "LLL") {
-      return "E";
-    }
+    const turnCount = command.length;
+    const directions = ["", "W", "S", "E"];
+    return directions[turnCount];
   }
 }
