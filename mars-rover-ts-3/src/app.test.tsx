e884eb17a81cd0acd0a992da9d44c8bbb5815223
jamiewallace <jwallacedev@gmail.com>
2025-09-10 11:19:11 +0100

    Failing app test for grid

--- mars-rover-ts-3/src/app.test.tsx
+++ mars-rover-ts-3/src/app.test.tsx
@@ -1,102 +1,113 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import App from "./App.tsx";
-import { MarsRoverController } from "./MarsRoverController";
-import { vi } from "vitest";
+import {MarsRoverController} from "./MarsRoverController";
+import {vi} from "vitest";
 import Mock = jest.Mock;
 
 vi.mock("./MarsRoverController");
+vi.mock("./MarsGrid", () => () => (
+    <span data-testid="grid"></span>
+));
 
 let executeFunction: Mock<any, any>;
 
 describe("App should", () => {
-  beforeEach(() => {
-    executeFunction = vi.fn();
-    MarsRoverController.prototype.execute = executeFunction;
-  });
+    beforeEach(() => {
+        executeFunction = vi.fn();
+        MarsRoverController.prototype.execute = executeFunction;
+    });
 
-  it("Start with an empty command list", () => {
-    render(<App />);
+    it("Render a grid", () => {
+        render(<App/>);
 
-    const commandString = screen.getByLabelText("Command:");
+        const grid = screen.getByTestId("grid");
 
-    expect(commandString).toHaveValue("");
-    expect(commandString).toBeDisabled();
-  });
+        expect(grid).toBeVisible();
+    });
 
-  it.each([
-    [1, "L"],
-    [2, "LL"],
-    [5, "LLLLL"],
-  ])(
-    "Adds L to the command when we click Left",
-    async (clickCount, expectedCommand) => {
-      render(<App />);
+    it("Start with an empty command list", () => {
+        render(<App/>);
 
-      clickLeftButton(clickCount);
+        const commandString = screen.getByLabelText("Command:");
 
-      assertCommandIs(expectedCommand);
-    }
-  );
+        expect(commandString).toHaveValue("");
+        expect(commandString).toBeDisabled();
+    });
 
-  it.each([
-    [1, "R"],
-    [2, "RR"],
-    [5, "RRRRR"],
-  ])(
-    "Adds R to the command when we click Right",
-    async (clickCount, expectedCommand) => {
-      render(<App />);
+    it.each([
+        [1, "L"],
+        [2, "LL"],
+        [5, "LLLLL"],
+    ])(
+        "Adds L to the command when we click Left",
+        async (clickCount, expectedCommand) => {
+            render(<App/>);
 
-      clickRightButton(clickCount);
+            clickLeftButton(clickCount);
 
-      assertCommandIs(expectedCommand);
-    }
-  );
+            assertCommandIs(expectedCommand);
+        }
+    );
 
-  it("sends an empty execute command to the controller", () => {
-    render(<App />);
+    it.each([
+        [1, "R"],
+        [2, "RR"],
+        [5, "RRRRR"],
+    ])(
+        "Adds R to the command when we click Right",
+        async (clickCount, expectedCommand) => {
+            render(<App/>);
 
-    clickExecuteButton();
+            clickRightButton(clickCount);
 
-    expect(executeFunction).toHaveBeenCalledTimes(1);
+            assertCommandIs(expectedCommand);
+        }
+    );
 
-    expect(executeFunction).toHaveBeenCalledWith("");
-  });
+    it("sends an empty execute command to the controller", () => {
+        render(<App/>);
 
-  it("sends execute command of M to the controller", () => {
-    render(<App />);
+        clickExecuteButton();
 
-    clickLeftButton();
+        expect(executeFunction).toHaveBeenCalledTimes(1);
 
-    clickExecuteButton();
+        expect(executeFunction).toHaveBeenCalledWith("");
+    });
 
-    expect(executeFunction).toHaveBeenCalledTimes(1);
-    expect(executeFunction).toHaveBeenCalledWith("L");
-  });
+    it("sends execute command of M to the controller", () => {
+        render(<App/>);
+
+        clickLeftButton();
+
+        clickExecuteButton();
+
+        expect(executeFunction).toHaveBeenCalledTimes(1);
+        expect(executeFunction).toHaveBeenCalledWith("L");
+    });
 });
 
 function clickLeftButton(clickCount: number = 1) {
-  const button = screen.getByRole("button", { name: "Left" });
-  clickButton(button, clickCount);
+    const button = screen.getByRole("button", {name: "Left"});
+    clickButton(button, clickCount);
 }
 
 function clickRightButton(clickCount: number = 1) {
-  const button = screen.getByRole("button", { name: "Right" });
-  clickButton(button, clickCount);
+    const button = screen.getByRole("button", {name: "Right"});
+    clickButton(button, clickCount);
 }
 
 function clickExecuteButton() {
-  const button = screen.getByRole("button", { name: "Execute" });
-  clickButton(button, 1);
+    const button = screen.getByRole("button", {name: "Execute"});
+    clickButton(button, 1);
 }
 
 function clickButton(button: HTMLElement, clickCount: number = 1) {
-  for (let i = 0; i < clickCount; i++) {
-    fireEvent.click(button);
-  }
+    for (let i = 0; i < clickCount; i++) {
+        fireEvent.click(button);
+    }
 }
 
 function assertCommandIs(expectedCommand: string) {
-  const commandString = screen.getByLabelText("Command:");
-  expect(commandString).toHaveValue(expectedCommand);
+    const commandString = screen.getByLabelText("Command:");
+    expect(commandString).toHaveValue(expectedCommand);
 }
