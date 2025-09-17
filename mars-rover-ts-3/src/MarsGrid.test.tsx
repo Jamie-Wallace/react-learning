6dd4c554de7e23da6e17b6840ea4556c72aab925
Hazel Russom <hazel.russom@gmail.com>
2025-09-17 11:33:20 +0100

    Update comments

--- mars-rover-ts-3/src/MarsGrid.test.tsx
+++ mars-rover-ts-3/src/MarsGrid.test.tsx
@@ -9,7 +9,7 @@ describe("MarsGrid should", () => {
     });
 
     it.each(
-        [0, 1, 2]
+        [0, 1, 2, 3, 9]
     )
     ('Render square inside grid at x coordinate %s', (xCoord: number) => {
         render(<MarsGrid />);
@@ -18,7 +18,7 @@ describe("MarsGrid should", () => {
         expect(within(grid).getByLabelText(`square at x${xCoord} y0`)).toBeVisible();
     });
 
-    // Render each square
+    // Do we want to test that we don't render 10,10?
     // Do we want to pass in size?
     // Do we want to render rover?
     // Do we want to test grid layout CSS?
--- mars-rover-ts-3/src/MarsGrid.tsx
+++ mars-rover-ts-3/src/MarsGrid.tsx
@@ -3,7 +3,7 @@ import { ReactElement } from "react";
 function MarsGrid() {
     const squares : ReactElement[] = [];
 
-    for(let xCoord = 0; xCoord < 3; xCoord++){
+    for(let xCoord = 0; xCoord < 10; xCoord++){
         squares.push(<span aria-label={`square at x${xCoord} y0`}/>);
     }
 
