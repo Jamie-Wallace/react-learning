0313f824a20ab2bd5059feba3342c227b04406a4
Jamie Wallace <jamie.wallace@codurance.com>
2025-10-08 11:21:37 +0100

    Refactor

--- mars-rover-ts-3/src/MarsGrid.tsx
+++ mars-rover-ts-3/src/MarsGrid.tsx
@@ -3,12 +3,15 @@ import {ReactElement} from "react";
 function MarsGrid() {
     const squares: ReactElement[] = [];
 
-    for (let yCoord = 0; yCoord < 10; yCoord++) {
+    function buildRow(yCoord: number) {
         for (let xCoord = 0; xCoord < 10; xCoord++) {
             squares.push(<span aria-label={`square at x${xCoord} y${yCoord}`} key={`${xCoord}-${yCoord}`}/>);
         }
     }
 
+    for (let yCoord = 0; yCoord < 10; yCoord++) {
+        buildRow(yCoord);
+    }
 
     return (<div aria-label={`Mars rover grid`}>
         {squares}
