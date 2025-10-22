d7071b58007c8e8f12a9bcf67bfe06005ecba22e
Hazel Russom <hazel.russom@gmail.com>
2025-10-22 10:49:19 +0100

    Expand x,y tests | Remove x test | Remove y test

--- mars-rover-ts-3/src/MarsGrid.test.tsx
+++ mars-rover-ts-3/src/MarsGrid.test.tsx
@@ -9,27 +9,7 @@ describe("MarsGrid should", () => {
     });
 
     it.each(
-        [0, 1, 2, 3, 9]
-    )
-    ('Render square inside grid at x coordinate %s', (xCoord: number) => {
-        render(<MarsGrid />);
-
-        const grid = screen.getByLabelText("Mars rover grid")
-        expect(within(grid).getByLabelText(`square at x${xCoord} y0`)).toBeVisible();
-    });
-
-    it.each(
-        [0, 1, 2, 3, 9]
-    )
-    ('Render square inside grid at y coordinate %s', (yCoord: number) => {
-        render(<MarsGrid />);
-
-        const grid = screen.getByLabelText("Mars rover grid")
-        expect(within(grid).getByLabelText(`square at x0 y${yCoord}`)).toBeVisible();
-    });
-
-    it.each(
-        [[0, 0]]
+        [[0, 0], [0, 1], [2, 0], [9, 9]]
     )
     ('Render square inside grid at coordinates %s,%s', (xCoord: number, yCoord: number) => {
         render(<MarsGrid />);
@@ -38,7 +18,6 @@ describe("MarsGrid should", () => {
         expect(within(grid).getByLabelText(`square at x${xCoord} y${yCoord}`)).toBeVisible();
     });
 
-    // Do we want to combine the render square at x and y tests into a single render at x,y test?
     // Do we want to pass in size?
     // Do we want to test that we don't render 10,10?
     // Do we want to render rover?
